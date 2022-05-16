97440a2bc652732639f73bab56801293213e2645
mina <m1nna@naver.com>
2022-05-16 21:13:02 +0900

    MariaDB Connection

---
+++ src/test/java/org/zerock/ex2/Ex2ApplicationTests.java
@@ -0,0 +1,14 @@
+package org.zerock.ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("contextLoads...");
+    }
+
+}
---
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -0,0 +1,47 @@
+package org.zerock.ex2.repository;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.ex2.entity.Memo;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class MemoRepositoryTests {
+
+    @Autowired
+    MemoRepository memoRepository;
+
+    @Test
+    public void testClass(){
+
+        System.out.println(memoRepository.getClass().getName());
+
+    }
+
+    @Test
+    public void testInsertDummies() {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Memo memo = Memo.builder().momoText("Sample..." + i).build();
+            memoRepository.save(memo);
+        });
+    }
+
+    @Test
+    public void testSelect(){
+
+        // 데이터베이스에 존재하는 mno
+        Long mno = 100L;
+        Optional<Memo> result=memoRepository.findById(mno);
+
+        System.out.println("=================================");
+
+        if(result.isPresent()){
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
+    }
+}
