05708ad1727465f0a96fee83adc8f24055ee9118
mina <m1nna@naver.com>
2022-07-11 13:15:46 +0900

    AOP 적용

---
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,25 @@
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component //를 통해 Bean으로 등록하기 -> 그러나 스프링 Bean(Spring Config파일에 @Bean을 통해)에 등록해서 쓰는걸 더욱 선호
+public class TimeTraceAop {
+
+    @Around("execution(* hello.hellospring..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
+
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+}
--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-//구현체 없이 인터페이스를 통해서만 주입!
+//구현체 없이 인터페이스를 통해서만 구현!
 //Sping Data JPA가 JpaRepository를 보고 구현체를 자동으로 만들어 스프링 Bean에 자동으로 등록
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository{
 
